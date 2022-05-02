287aeb6c18ac8867c35e74d7aa522edf781c2cc7
Jacky Liang <asheriffy@gmail.com>
2022-05-02 13:48:28 -0400

    rough example of using raw SQL instead of an ORM

--- app.rb
+++ app.rb
@@ -1,13 +1,15 @@
-db = SQLite3::Database.new "game.db"
+require "sqlite3"
 
-db.execute <<-SQL
+$db = SQLite3::Database.new "game.db"
+
+$db.execute <<-SQL
     create table if not exists party (
         id interger primary key,
         name text
     );
 SQL
 
-db.execute <<-SQL
+$db.execute <<-SQL
     create table if not exists party (
         id interger primary key,
         name text,
@@ -24,3 +26,27 @@ db.execute <<-SQL
         party_id integer
     );
 SQL
+
+class Unit
+    attr_accessor :name, :hp, :mp, :job, :move, :attack, :defense
+    def initialize(name)
+        @name = name
+        @hp = 100
+        @mp = 25
+        @job = 'Squire'
+        @move = 3
+        @attack = 10
+        @defense = 5
+    end
+
+    def save
+        $db.execute("INSERT INTO units(name, hp, mp, job, move)
+        VALUES('#{@name}', '#{@hp}', '#{@mp}', '#{@job}', '#{@move}')
+        ")
+    end
+
+end
+
+
+
+## if a unit gets created successfully, we will save it to the database
